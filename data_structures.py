981656b0b0a6e32676451f2cdc23ca6b437e24bb
Alex Morris <alexrmorris88@gmail.com>
2021-07-16 21:07:46 -0300

    Tree Data Structure

--- data_structures.py
+++ data_structures.py
@@ -425,14 +425,82 @@ class PriorityQueue:
                 temp = temp.nextNode
 
 
-pq = PriorityQueue()
-pq.push(4, 1)
-pq.push(5, 2)
-pq.push(6, 3)
-pq.push(7, 0)
+# pq = PriorityQueue()
+# pq.push(4, 1)
+# pq.push(5, 2)
+# pq.push(6, 3)
+# pq.push(7, 0)
+#
+# pq.pop()
+#
+# pq.traverse()
 
-pq.pop()
 
-pq.traverse()
+# ===================================================================================
+# General Trees
+# ===================================================================================
+"""
+this tutorial is from https://www.youtube.com/watch?v=4r_XR9fUPhQ
+"""
+
+
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.child = []
+        self.parent = None
+
+    def add_child(self, child):
+        child.parent = self
+        self.child.append(child)
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+
+        return level
+
+    def print_tree(self):
+        spaces = '  ' * self.get_level()
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix + self.data)
+        if len(self.child) > 0:
+            for c in self.child:
+                c.print_tree()
+
+def build_product_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("Samsung"))
+    tv.add_child(TreeNode("LG"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    return root
+
+
+root = build_product_tree()
+root.print_tree()
+
+
+
+
+
 
 
