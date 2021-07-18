96d00ad93212b0acf3a011bf08b5a59f3ff6318e
Alex Morris <alexrmorris88@gmail.com>
2021-07-18 10:56:41 -0300

    heap data structure

--- data_structures.py
+++ data_structures.py
@@ -571,8 +571,68 @@ def build_tree(elements):
 
 
 # ===================================================================================
-# Binary Trees
+# Heap - MaxHeap
 # ===================================================================================
 """
-this tutorial is from 
+this tutorial is from https://www.section.io/engineering-education/heap-data-structure-python/
 """
+
+
+class MaxHeap:
+    def __init__(self):
+        # Initialize a heap using list
+        self.heap = []
+
+    def getParentPosition(self, i):
+        # The parent is located at floor((i-1)/2)
+        return int((i-1)/2)
+
+    def getLeftChildPosition(self, i):
+        # The left child is located at 2 * i + 1
+        return 2*i+1
+
+    def getRightChildPosition(self, i):
+        # The right child is located at 2 * i + 2
+        return 2*i+2
+
+    def hasParent(self, i):
+        # This function checks if the given node has a parent or not
+        return self.getParentPosition(i) < len(self.heap)
+
+    def hasLeftChild(self, i):
+        # This function checks if the given node has a left child or not
+        return self.getLeftChildPosition(i) < len(self.heap)
+
+    def hasRightChild(self, i):
+        # This function checks if the given node has a right child or not
+        return self.getRightChildPosition(i) < len(self.heap)
+
+    def getMax(self):
+        return self.heap[0] # Returns the largest value in the heap in O(1) time.
+
+    def insert(self, key):
+        self.heap.append(key) # Adds the key to the end of the list
+        self.heapify(len(self.heap) - 1) # Re-arranges the heap to maintain the heap property
+
+    def heapify(self, i):
+        while(self.hasParent(i) and self.heap[i] > self.heap[self.getParentPosition(i)]): # Loops until it reaches a leaf node
+            self.heap[i], self.heap[self.getParentPosition(i)] = self.heap[self.getParentPosition(i)], self.heap[i] # Swap the values
+            i = self.getParentPosition(i) # Resets the new position
+
+    def printHeap(self):
+        print(self.heap) # Prints the heap
+
+
+h = MaxHeap()
+h.insert(2)
+h.insert(100)
+h.insert(20)
+h.insert(40)
+h.insert(5)
+h.insert(8)
+h.insert(19)
+
+h.printHeap()
+
+print(h.getMax())
+
