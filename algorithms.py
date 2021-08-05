e2bb23c43a96a1e33ad4f12a0d3e625e2d551f85
Alex Morris <alexrmorris88@gmail.com>
2021-08-05 13:46:47 -0300

    Merge Sort Algorithm

--- algorithms.py
+++ algorithms.py
@@ -12,9 +12,60 @@ def BubbleSort(x):
                 x[j], x[j + 1] = x[j + 1], x[j]
 
 
-x = [22, 4, 1, 500, 7, 9, 2000, 5]
+# x = [22, 4, 1, 500, 7, 9, 2000, 5]
+#
+# BubbleSort(x)
+#
+# for i in range(len(x)):
+#     print(x[i], end=" ")
 
-BubbleSort(x)
 
-for i in range(len(x)):
-    print(x[i], end=" ")
+# =============================================================================
+# Merge Sort Algorithm
+# =============================================================================
+
+
+def MergeSort(x):
+    if len(x) > 1:
+        mid = len(x) // 2
+        L = x[:mid]
+        R = x[mid:]
+
+        MergeSort(L)
+        MergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                x[k] = L[i]
+                i += 1
+                k += 1
+            else:
+                x[k] = R[j]
+                j += 1
+                k += 1
+
+        while i < len(L):
+            x[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            x[k] = R[j]
+            j += 1
+            k += 1
+
+
+def printList(x):
+    for i in range(len(x)):
+        print(x[i], end=" ")
+    print()
+
+if __name__ == "__main__":
+    x = [12, 11, 13, 15, 7, 5, 10, 1, 4, 3, 9]
+    printList(x)
+    MergeSort(x)
+    printList(x)
+
+
