d94092011873cb425ee5b790d9dd566fea578203
Alex Morris <alexrmorris88@gmail.com>
2021-08-15 11:12:23 -0300

    Stright Forward approach

--- algorithms.py
+++ algorithms.py
@@ -153,10 +153,10 @@ def quick_sort(start, end, array):
         quick_sort(p + 1, end, array)
 
 
-if __name__ == "__main__":
-    array = [ 1000, 10, 7, 8, 9, 1, 5, 100, 88, 33, 6, 11, 20, 30, 40 ]
-    quick_sort(0, len(array) - 1, array)
-    print(f'Sorted array: {array}')
+# if __name__ == "__main__":
+#     array = [ 1000, 10, 7, 8, 9, 1, 5, 100, 88, 33, 6, 11, 20, 30, 40 ]
+#     quick_sort(0, len(array) - 1, array)
+#     print(f'Sorted array: {array}')
 
 
 # =============================================================================
@@ -191,8 +191,43 @@ def heapSort(arr):
         heapify(arr, i, 0)
 
 
+# if __name__ == "__main__":
+#     arr = [12, 11, 13, 5, 6, 7]
+#     heapSort(arr)
+#     for i in range(len(arr)):
+#         print(arr[i], end=" ")
+
+
+# =============================================================================
+# Finding a Peak - Stright Forward Approach
+# =============================================================================
+
+
+def findPeak(array, n):
+    if(n == 1):
+        return 0
+    if(array[0] >= array[1]):
+        return 0
+    if(array[n - 1] >= array[n - 2]):
+        return n - 1
+
+    for i in range(1, n - 1):
+        if(array[i] >= array[i - 1] and array[i] >= array[i + 1]):
+            return i
+
+
 if __name__ == "__main__":
-    arr = [12, 11, 13, 5, 6, 7]
-    heapSort(arr)
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
+    array = [ 1, 3, 5, 66, 9, 5, 77, 2, 10, 100, 90]
+    n = len(array)
+    print(f"The peak is {findPeak(array, n)}")
+
+
+
+
+
+
+
+
+
+
+
